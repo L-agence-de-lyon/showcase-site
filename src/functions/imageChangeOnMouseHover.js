93b19cfca9b99fbb910b0f924824c2f82c649a5a
Franck <francklucasehui@gmail.com>
2022-11-30 22:10:35 +0100

    FIX: image background transition

--- src/functions/imageChangeOnMouseHover.js
+++ src/functions/imageChangeOnMouseHover.js
@@ -1,32 +1,26 @@
 let currImage = "services";
-let isChanging = false;
 const imageChangeOnMouseHover = async (src) => {
-  if (currImage === src || isChanging) return;
+  if (currImage === src) return;
   else {
-    isChanging = true;
     const image = document.getElementsByClassName("menu-image")[0];
-    await transition(image);
     var img = require.context("../assets/", false, /\.png$/);
-    image.src = img("./" + src + ".png");
-    await transition(image);
-    image.style.opacity = 1;
+    image.style.backgroundImage = `url(${img("./" + src + ".png")})`;
     currImage = src;
-    isChanging = false;
   }
 };
 
-function transition(img) {
-  return new Promise(function (resolve) {
-    var del = 0.01;
-    var id = setInterval(changeOpacity, 2);
-    function changeOpacity() {
-      img.style.opacity -= del;
-      if (img.style.opacity <= 0) {
-        clearInterval(id);
-        resolve();
-      }
-    }
-  });
-}
+// function transition(img) {
+//   return new Promise(function (resolve) {
+//     var del = 0.01;
+//     var id = setInterval(changeOpacity, 1);
+//     function changeOpacity() {
+//       img.style.opacity -= del;
+//       if (img.style.opacity <= 0) {
+//         clearInterval(id);
+//         resolve();
+//       }
+//     }
+//   });
+// }
 
 export { imageChangeOnMouseHover };
