11949bb65ba3a13a73aa38bbfc05830a839dc924
Gregoire63 <gregoire.raturat@laposte.net>
2022-12-02 10:44:52 +0100

    feat: swiper scroll/ custom item per row cardstats

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
+   <swiper
+    :direction="'vertical'"
+    :slidesPerView="'auto'"
+    :freeMode="true"
+    :scrollbar="{
+      draggable: true,
+      hide:true
+    }"
+    :mousewheel="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>
   <Nav />
   <MenuVue />
   <router-view class="px-11 mt-20" />
+</swiper-slide>
+  </swiper>
+  
 </template>
 
 <script>
@@ -9,11 +25,25 @@ import MenuVue from "./components/menu/Menu.vue";
 import Nav from "./components/nav/Nav.vue";
 import "./styles/app.css";
 
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/scrollbar";
+import "@/styles/swiper.css";
+import { FreeMode, Scrollbar, Mousewheel } from "swiper";
+
 export default {
   name: "App",
   components: {
     Nav,
     MenuVue,
+    SwiperSlide,
+    Swiper,
+  },
+  setup() {
+    return {
+      modules: [FreeMode, Scrollbar, Mousewheel],
+    };
   },
 };
 </script>
--- src/components/card/StatsCard.vue
+++ src/components/card/StatsCard.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="w-full p-4 text-center bg-white border rounded-lg shadow-md sm:p-8 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-600 via-blue-900 to-black from-black via-violet-800 to-gray-800 from-sky-400 to-indigo-900 dark:border-gray-700 flex items-center space-x-4">
+    <div :class="[`lg:grid-cols-${countPerRow?countPerRow:3}`]" class="w-full text-center mb-5 bg-white border rounded-lg shadow-md p-8 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-blue-600 via-blue-900 to-black dark:border-gray-700 grid sm:grid-cols-1 md:grid-cols-2 gap-4 ">
         <div v-for="content of contents" :key="content.title" >
-            <component :is="content.icon" class="h-20 w-20 text-white m-auto">
+            <component :is="content.icon" class="lg:h-20 lg:w-20 w-10 h-10 text-white m-auto">
                 <slot />
             </component>
             <h5 class="mb-5 text-3xl font-bold text-gray-900 dark:text-white">{{content.title}}</h5>
@@ -12,6 +12,6 @@
 
 <script>
 export default {
-    props: ['contents'],
+    props: ['contents','countPerRow'],
 }
 </script>
